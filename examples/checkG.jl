b85d933aa0623a05f5bff604f7acb2dd8f52aab9
jerryho <cheukhinhojerry@gmail.com>
2022-11-08 20:11:17 -0500

    add plot

--- examples/checkG.jl
+++ examples/checkG.jl
@@ -1,8 +1,10 @@
 include("../src/utils.jl")
 # include("../ACEcore.jl/src/ACEcore.jl")
+using Distributions
+using Plots
 
 max_degree = 15
-N = 1000
+N = 10000
 
 # 2 body case
 legendre(N) = legendre_basis(N, normalize=true) 
@@ -13,21 +15,21 @@ G2b = Data' * Data
 
 
 # 3 body case
-X1 = LinRange(-1, 1, N)
-X2 = LinRange(-1, 1, N)
 NN = get_NN(max_degree)
 poly = legendre_basis(max_degree, normalize = true)
-G = getG(X1, X2, poly, NN[max_degree + 1:end])
-println("cond(G): ", cond(G))
-
-
-
+X1, X2 = rand(Uniform(-1, 1), N), rand(Uniform(-1, 1), N)
+S = length(NN)
 ground_pure2b = zeros(N, S - max_degree)
+poly_X1 = poly(X1)
+poly_X2 = poly(X2)
 # see_NN = []
 for i = 1:S - max_degree
-    # nn, mm = NN[i + max_degree]
-    #@show nn, mm
-    push!(see_NN, NN[i + max_degree])
-    ground_pure2b[:, i] = poly(X1)[:, nn] .* poly(X2)[:, mm] + poly(X1)[:, mm] .* poly(X2)[:, nn]
+    nn, mm = NN[i + max_degree]
+    ground_pure2b[:, i] = (poly_X1[:, nn] .* poly_X2[:, mm] + poly_X1[:, mm] .* poly_X2[:, nn])
 end
+M = ground_pure2b
 @show cond(ground_pure2b)
+
+# plot the matrix 
+heatmap(ground_pure2b' * ground_pure2b , aspect_ratio=1)
+savefig("check_ground_pure2b")
