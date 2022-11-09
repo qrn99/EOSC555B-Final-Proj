fb6ed48d3a03004d94777b118080f168acfe08bd
Ruo Ning Qiu <ruoning99@hotmail.com>
2022-11-09 15:59:55 -0800

    Draft spliting A^TA into two sums

--- examples/2bCondnoG.jl
+++ examples/2bCondnoG.jl
@@ -1,29 +1,9 @@
 include("../src/utils.jl")
 using LinearAlgebra, Polynomials4ML
-# function legendre(x::Number, N)
-#     L = zeros(N+1)
-#     L[1] = 1
-#     L[2] = x
-#     for n = 2:N
-#         L[n+1] = (2*n-1)/n * x * L[n] - (n-1)/n * L[n-1]
-#     end
-#     for n = 0:N
-#         L[n+1] *= 2*n+1
-#     end
-#     return L
-#  end
- 
-#  function cheb(x::Number, N)
-#     T = zeros(N+1)
-#     T[1] = 1 
-#     T[2] = x 
-#     for n = 2:N 
-#        T[n+1] = 2*x*T[n] - T[n-1]
-#     end 
-#     return T 
-# end
+##
 cheb(N) = chebyshev_basis(N)
 legendre(N) = legendre_basis(N, normalize=true) 
+##
 
 # r_cut = 1
 # r_in = -1
@@ -78,6 +58,7 @@ let N = 10, M=1000, K_R = 1
     @show cond(A'*A)
 end
 
+## 
 # averaged 2B
 let Ns = [10, 20], Ms = [20, 100, 1000], K_Rs = [1, 2, 4], basis_list = [cheb, legendre]
     for basis in basis_list
@@ -99,7 +80,9 @@ let Ns = [10, 20], Ms = [20, 100, 1000], K_Rs = [1, 2, 4], basis_list = [cheb, l
         end
     end
 end
+##
 
+##
 # For the A^TA LSQ stability of averaged energy design matrix A  
 # Check unknown prodcut term behaviour as M → ∞
 let Ns = [20], Ms = [20, 100, 1000], K_Rs = [1, 2, 4], basis_list = [legendre]
@@ -110,37 +93,107 @@ let Ns = [20], Ms = [20, 100, 1000], K_Rs = [1, 2, 4], basis_list = [legendre]
             for K_R in K_Rs
                 for M in Ms
                     Rs = [rand(K_R)*2 .- 1 for _=1:M]
-                    sum1 = 0
-                    sum2 = 0
-                    n = 1
-                    n2 = 1
-                    for (i, rr) in enumerate(Rs)
-                        for k=eachindex(rr)
-                            for k2=eachindex(rr)
-                                if k == k2
-                                    # @show evaluate(cheb(N), rr[k])
-                                    sum1 += evaluate(cheb(N), rr[k])[n] * evaluate(cheb(N), rr[k'])[n2]
-                                else
-                                    sum2 += evaluate(cheb(N), rr[k])[n] * evaluate(cheb(N), rr[k'])[n2]
+                    sum1_mat = zeros(N, N)
+                    sum2_mat = zeros(N, N)
+                    for n=1:N
+                        for n2=1:N
+                            sum1 = 0
+                            sum2 = 0
+                            for (i, rr) in enumerate(Rs)
+                                for k=eachindex(rr)
+                                    for k2=eachindex(rr)
+                                        if k == k2
+                                            # @show evaluate(cheb(N), rr[k])
+                                            sum1 += evaluate(cheb(N), rr[k])[n] * evaluate(cheb(N), rr[k'])[n2]
+                                        else
+                                            sum2 += evaluate(cheb(N), rr[k])[n] * evaluate(cheb(N), rr[k'])[n2]
+                                        end
+                                    end
                                 end
                             end
+                            sum1_mat[n, n2] = sum1
+                            sum2_mat[n, n2] = sum2
                         end
                     end
-                    @show (M, N, K_R, (n, n2))
-                    @show sum1
-                    # @show sum1/(K_R^2)
-                    @show sum2
-                    # @show sum2/(K_R^2)
+                    @show (M, N, K_R)
+                    heatmap(sum1_mat/(K_R^2), aspect_ratio=1)
+                    heatmap(sum2_mat/(K_R^2), aspect_ratio=1)
                 end
             end
         end
     end
 end
+##
 
+##
+function getSumDiffProduct(Rs, basis, N, K_R)
+    basis_eval = basis(N)
+    sum1_mat = zeros(N, N)
+    sum2_mat = zeros(N, N)
+    for (i, rr) in enumerate(Rs)
+        sum1_i = zeros(N, N)
+        sum2_i = zeros(N, N)
+        for k=eachindex(rr)
+            for k2=eachindex(rr)
+                if k == k2
+                    # outer product
+                    sum1_i += evaluate(basis_eval, rr[k]) .* evaluate(basis_eval, rr[k'])'
+                else
+                    sum2_i += evaluate(basis_eval, rr[k]) .* evaluate(basis_eval, rr[k'])'
+                end
+            end
+        end
+        sum1_mat += sum1_i 
+        sum2_mat += sum2_i
+    end
+    return sum1_mat/(K_R^2), sum2_mat/(K_R^2)
+end
+
+# function des_mat2(Rs, basis, N, K_R)
+#     basis_eval = basis(N)
+#     sum2 = zeros(N, N)
+#     for (i, rr) in enumerate(Rs)
+#         @show sum(evaluate(basis_eval, r) for r in rr)
+#         sum2 += sum(evaluate(basis_eval, r) for r in rr) * sum(evaluate(basis_eval, r) for r in rr)'
+#     end
+#     return sum2/(K_R^2)
+# end
+# A2 = des_mat2(Rs, legendre, N, K_R)
+
+function design_matrix_no_scale(xs, basis, N)
+    A = zeros(ComplexF64, length(xs), N)
+    for (i, xx) in enumerate(xs)
+      A[i, :] = sum(evaluate(basis(N), x) for x in xx)/ length(xx)
+    end
+    return A
+end
+
+N = 10
+M = 100
+K_R = 2
+Rs = [rand(K_R)*2 .- 1 for _=1:M]
+sum1, sum2 = getSumDiffProduct(Rs, legendre, N, K_R)
+A = real(design_matrix(Rs, legendre, N))
+# A2 = des_mat2(Rs, legendre, N, K_R)
+# A'*A .== A2
+A'*A
+A'*A .== sum1+sum2
+@assert(A'*A == sum1+sum2)
+A'*A
+sum1+sum2
+@show (sum1+sum2)
+heatmap(sum1 , aspect_ratio=1)
+heatmap(sum2, aspect_ratio=1)
+heatmap(A'*A, aspect_ratio=1)
+##
+
+
+##
 # simple 2B
 N = 10
 M = 100
-Xs = LinRange(-1, 1, M)
+# Xs = LinRange(-1, 1, M)
+Xs = rand(N)*2 .- 1
 # Data = cheb(N)(Xs)
 # @show size(Data)
 
@@ -167,13 +220,18 @@ let Ns = [10, 20], Ms = [20, 100, 1000], basis_list = [cheb, legendre]
         end
     end
 end
-
+#%%
 
 # simple 3B
 N = 1000 #num of samples
 max_degree = 15
 X1 = LinRange(-1, 1, N)
+X1 = collect(LinRange(-1, 1, N))
+
+# X1 = rand(N) .* 2 .- 1
 X2 = LinRange(-1, 1, N)
+X2 = collect(LinRange(-1, 1, N))
+# X2 = rand(N) .* 2 .- 1
 NN = get_NN(max_degree)
 
 function G_innerProd_3b(X1, X2, poly, NN2)
@@ -192,5 +250,7 @@ end
 
 poly = legendre_basis(max_degree, normalize = true)
 G = G_innerProd_3b(X1, X2, poly, NN[max_degree + 1:end])
+@show cond(G)
 @show "||| M ||| ",  sqrt(1/N * norm(G, 2))
 
+##
