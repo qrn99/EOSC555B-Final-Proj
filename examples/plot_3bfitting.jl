926fcc71a4221cdf5baafe2adacef5fd8aa631db
Ruo Ning Qiu <ruoning99@hotmail.com>
2022-11-30 23:29:48 -0800

    Fix bug

--- examples/plot_3bfitting.jl
+++ examples/plot_3bfitting.jl
@@ -7,10 +7,14 @@ using PyCall
 
 f1(x) = 1/(1+8*x^2)
 f2(x) = abs(x)^3
+
+f1_V2(x, y) = x^2 + y^2
+f2_V2(X) = sum([X[:, i].^2 - 10 * cos.(2 * pi * X[:, i]) .+ 10 for i = 1:size(X)[2]])
+
 # E_avg(X, f) = sum([f.(X[:, i]) for i = 1:size(X)[2]])
 E_avg(X, f) = sum([f.(X[:, i])/length(size(X)[2]) for i = 1:size(X)[2]])
 
-V2(X, f) = [f(X[j, :][1])+f(X[j, :][2])  for j = 1:size(X)[1]]
+V2(X, f_V2) = [f_V2(X[j, :][1], X[j, :][2]) for j = 1:size(X)[1]]
 
 # function E_avg3b(X, f)
 #     for i = eachindex()
@@ -35,7 +39,7 @@ noise=0
 
 solver = :qr
 
-f = f2
+f = f1_V2
 Testing_func(X) = V2(X, f)
 poly = legendre_basis(max_degree, normalize = true)
 
@@ -46,22 +50,33 @@ A_pure = designMatNB(X, poly, max_degree, ord; body = body_order)
 
 sol_pure = solveLSQ(A_pure, Y; solver=solver)
 
-XX_test = range(domain_lower, domain_upper, length=testSampleSize)
+# XX_test = range(domain_lower, domain_upper, length=testSampleSize)
 
-A_test = predMatNB(XX_test, poly, max_degree, ord; body = body_order)
+XX_test = rand(distribution(domain_lower, domain_upper), (testSampleSize, K_R))
+XX_test = XX_test[sortperm(XX_test[:, 1]), :]
+
+# XX_test_r1 = sort(rand(distribution(domain_lower, domain_upper), M))
+# XX_test_r2 = sort(rand(distribution(domain_lower, domain_upper), M))
+
+# A_test = predMatNB(XX_test, poly, max_degree, ord; body = body_order)
+A_test = designMatNB(XX_test, poly, max_degree, ord; body = body_order)
 yp = A_test * sol_pure
-ground_yp = f.(XX_test)
+ground_yp = V2(XX_test, f)
 
 println("relative error of pure basis: ", norm(yp - ground_yp)/norm(ground_yp))
 println("RMSE: ", norm(yp - ground_yp)/sqrt(M))
 
 target_x = range(domain_lower, domain_upper, length=500)
-p = plot(target_x, f.(target_x), c=1,
+target_y = range(domain_lower, domain_upper, length=500)
+target_z = f1_V2.(target_x, target_y)
+# V2(hcat(target_x, target_y)
+p = plot(target_x, target_y, f, st=:surface,
 #                         xlim=[-1.1, 1.1], ylim=[-1, 2],
             size = (1000, 800),
             label = "target", xlabel="x", ylabel="f(x)", title="order=$ord, basis maxdeg = $max_degree, sample size = $M, K_R=$K_R")
 training_flatten = reduce(vcat, X)
 test_flatten = reduce(vcat, XX_test)
-plot!(training_flatten, f.(training_flatten), c=1, seriestype=:scatter, m=:o, ms=1, label = "")
+scatter!(X[:, 1], X[:, 2], Y, seriestype=:scatter, m=:o, ms=1, label = "")
 plot!(XX_test, yp, c=2, ls=:dash, label = "prediction")
 p
+
