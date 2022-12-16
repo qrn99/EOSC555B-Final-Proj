7f55449a067b27eced456f9f888ba3675dbbc240
jerryho <cheukhinhojerry@gmail.com>
2022-12-15 22:39:45 -0500

    add savefig

--- examples/plot_2bfitting.jl
+++ examples/plot_2bfitting.jl
@@ -16,14 +16,18 @@ E(f, Rs::Vector{Vector{Float64}}) = [ sum(f.(R))/length(R) for R in Rs ]
 #     rdf_bimodal = range(0.15, 0.3, 16), K_R = [1, 4, 16, 64], noise=[0.0, 1e-4, 1e-3] #, 1e-2, 1e-1]
 
 BasisDeg=20
-M=40
+
+num_data = 100
+K_R = 5
+
+M = floor(num_data / K_R)
+
 testSampleSize=400
 test_uniform=true
 adaptedTrainSize=testSampleSize
 
 domain_lower=-1
 domain_upper=1
-K_R = 4
 noise=1e-4
 
 let fs=[f1, f2]
@@ -49,16 +53,16 @@ let fs=[f1, f2]
         for basis_choice in basis_choices
             for dst in distributions 
                 train, test = HelperFunctions.generate_data(M, K_R, testSampleSize, data_dst, dst)
-
+                #@show size(train)
                 training_flatten = sort(collect(Iterators.flatten(train)))
     #                     xp_pp_test = sort(collect(Iterators.flatten(test)))
+                #@show size(training_flatten)
                 xp_pp_test = range(domain_lower, domain_upper, length=testSampleSize)
 
                 basis = HelperFunctions.get_basis(basis_choice, BasisDeg, K_R, data_dst, dst; adaptedTrainSize=adaptedTrainSize)
                 
                 μ = HelperFunctions.lr(E, f, train, basis, N; noise=noise)
                 yp = HelperFunctions.predict(xp_pp_test, basis, μ)
-
                 target_x = range(-1, 1, length=200)
                 p = plot(target_x, f.(target_x), c=1,
     #                         xlim=[-1.1, 1.1], ylim=[-1, 2],
@@ -71,63 +75,64 @@ let fs=[f1, f2]
         end
     end
     l = @layout [grid(length(fs)+length(basis_choices)+2, length(distributions))]
-    plot(plots..., layout = l, size=(2500, 1500), plot_title="deg=$BasisDeg, K_R = $K_R, noise=$noise, test_uniform=$test_uniform")
+    PL = plot(plots..., layout = l, size=(2500, 1500), plot_title="deg=$BasisDeg, K_R = $K_R, noise=$noise, test_uniform=$test_uniform")
+    savefig(PL, "plots_KR=" * string(K_R))
 end
 ##
 
-let
-    f1(x) = 1/(1+8*x^2)
-    f2(x) = abs(x)^3
-    # E_avg(X, f) = sum([f.(X[:, i]) for i = 1:size(X)[2]])
-    E_avg(X, f) = sum([f.(X[:, i])/length(size(X)[2]) for i = 1:size(X)[2]])
+# let
+#     f1(x) = 1/(1+8*x^2)
+#     f2(x) = abs(x)^3
+#     # E_avg(X, f) = sum([f.(X[:, i]) for i = 1:size(X)[2]])
+#     E_avg(X, f) = sum([f.(X[:, i])/length(size(X)[2]) for i = 1:size(X)[2]])
 
-    M = 100
-    max_degree = 20
-    ord = 1 #2b+3b, can access 3b only 
-    body_order = :TwoBody
+#     M = 100
+#     max_degree = 20
+#     ord = 1 #2b+3b, can access 3b only 
+#     body_order = :TwoBody
 
-    testSampleSize=400
-    test_uniform=true
-    distribution=Uniform
+#     testSampleSize=400
+#     test_uniform=true
+#     distribution=Uniform
 
-    domain_lower=-1
-    domain_upper=1
-    K_R = 4
+#     domain_lower=-1
+#     domain_upper=1
+#     K_R = 4
 
-    noise=0
-    # noise=1e-4
+#     noise=0
+#     # noise=1e-4
 
-    solver = :qr
+#     solver = :qr
 
-    f = f2
-    Testing_func(X) = E_avg(X, f)
-    poly = legendre_basis(max_degree, normalize = true)
+#     f = f2
+#     Testing_func(X) = E_avg(X, f)
+#     poly = legendre_basis(max_degree, normalize = true)
 
-    X = rand(distribution(domain_lower, domain_upper), (M, K_R))
-    Y = Testing_func(X) .+ noise
+#     X = rand(distribution(domain_lower, domain_upper), (M, K_R))
+#     Y = Testing_func(X) .+ noise
 
-    A_pure = designMatNB(X, poly, max_degree, ord; body = body_order)
+#     A_pure = designMatNB(X, poly, max_degree, ord; body = body_order)
 
-    sol_pure = solveLSQ(A_pure, Y; solver=solver)
+#     sol_pure = solveLSQ(A_pure, Y; solver=solver)
 
-    XX_test = range(domain_lower, domain_upper, length=testSampleSize)
+#     XX_test = range(domain_lower, domain_upper, length=testSampleSize)
 
-    A_test = predMatNB(XX_test, poly, max_degree, ord; body = body_order)
-    yp = A_test * sol_pure
-    ground_yp = f.(XX_test)
+#     A_test = predMatNB(XX_test, poly, max_degree, ord; body = body_order)
+#     yp = A_test * sol_pure
+#     ground_yp = f.(XX_test)
 
-    println("relative error of pure basis: ", norm(yp - ground_yp)/norm(ground_yp))
-    println("RMSE: ", norm(yp - ground_yp)/sqrt(M))
+#     println("relative error of pure basis: ", norm(yp - ground_yp)/norm(ground_yp))
+#     println("RMSE: ", norm(yp - ground_yp)/sqrt(M))
 
-    target_x = range(domain_lower, domain_upper, length=500)
-    p = plot(target_x, f.(target_x), c=1,
-    #                         xlim=[-1.1, 1.1], ylim=[-1, 2],
-                size = (1000, 800),
-                label = "target", xlabel="x", ylabel="f(x)", title="order=$ord, basis maxdeg = $max_degree, sample size = $M, K_R=$K_R")
-    training_flatten = reduce(vcat, X)
-    test_flatten = reduce(vcat, XX_test)
-    plot!(training_flatten, f.(training_flatten), c=1, seriestype=:scatter, m=:o, ms=1, label = "")
-    plot!(XX_test, yp, c=2, ls=:dash, label = "prediction")
-    p
-end
-##
+#     target_x = range(domain_lower, domain_upper, length=500)
+#     p = plot(target_x, f.(target_x), c=1,
+#     #                         xlim=[-1.1, 1.1], ylim=[-1, 2],
+#                 size = (1000, 800),
+#                 label = "target", xlabel="x", ylabel="f(x)", title="order=$ord, basis maxdeg = $max_degree, sample size = $M, K_R=$K_R")
+#     training_flatten = reduce(vcat, X)
+#     test_flatten = reduce(vcat, XX_test)
+#     plot!(training_flatten, f.(training_flatten), c=1, seriestype=:scatter, m=:o, ms=1, label = "")
+#     plot!(XX_test, yp, c=2, ls=:dash, label = "prediction")
+#     p
+# end
+# ##
