c24c33ccf0aacbe2a3efc0ca063e7895165f38f0
Ruo Ning Qiu <ruoning99@hotmail.com>
2023-03-26 21:58:16 -0700

    Fix scale of error

--- examples/2b_pp_error_and_visual.jl
+++ examples/2b_pp_error_and_visual.jl
@@ -129,7 +129,7 @@ end
 
 let
     Testing_func(X) = E_avg(X, f)
-    P = Plots.plot(xaxis  = (:log, "Sample Size"),
+    P = Plots.plot(xaxis  = ("Sample Size"),
                             yaxis  = (:log, "RMSE"), 
                             legend = :topright, 
                             size = (600, 500))
